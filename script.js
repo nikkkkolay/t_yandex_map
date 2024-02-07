d534a3e84baf1cfe29d62ec0356e904188bde132
Гагара Николай Федорович <ggn.f764@gmail.com>
2024-02-07 11:33:26 +0300

    добавил к маркера кастомный попап

--- script.js
+++ script.js
@@ -37,7 +37,91 @@ $(".navbar-collapse").on("click", function () {
 async function initMap() {
     await ymaps3.ready;
 
-    const { YMap, YMapDefaultSchemeLayer, YMapMarker, YMapDefaultFeaturesLayer } = ymaps3;
+    const markerPropsVO = [
+        {
+            coordinates: [33.06208599275346, 68.95469889784452],
+            iconSrc: "https://yastatic.net/s3/front-maps-static/maps-front-jsapi-3/examples/images/marker-custom-icon/yellow-capybara.png",
+            message: "I'm yellow capybara!",
+        },
+        {
+            coordinates: [33.0767053822784, 68.96478012321226],
+            iconSrc: "https://yastatic.net/s3/front-maps-static/maps-front-jsapi-3/examples/images/marker-custom-icon/yellow-capybara.png",
+            message: "I'm yellow capybara!",
+        },
+    ];
+
+    const { YMapComplexEntity, YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker } = ymaps3;
+
+    class CustomMarkerWithPopup extends YMapComplexEntity {
+        constructor(props) {
+            super(props);
+            this._popupOpen = false;
+        }
+
+        // Handler for attaching the control to the map
+        _onAttach() {
+            this._createPopup();
+            this._createMarker();
+            this._actualizePopup();
+        }
+        // Handler for detaching control from the map
+        _onDetach() {
+            this._marker = null;
+        }
+        // Handler for updating marker properties
+        _onUpdate(props) {
+            if (props.coordinates) {
+                this._marker?.update({ coordinates: props.coordinates });
+            }
+        }
+        // Method for updating the status of a popup window
+        _actualizePopup() {
+            if (this._popupOpen) {
+                this._popup.style.display = "flex";
+            } else {
+                this._popup.style.display = "none";
+            }
+        }
+        // Method for creating a marker element
+        _createMarker() {
+            const container = document.createElement("div");
+            const marker = document.createElement("img");
+            marker.className = "icon-marker";
+            marker.src = this._props.icon;
+            marker.className = "icon-marker";
+
+            marker.onclick = () => {
+                this._popupOpen = true;
+                this._actualizePopup();
+            };
+
+            container.append(marker, this._popup);
+
+            this._marker = new YMapMarker({ coordinates: this._props.coordinates }, container);
+            this.addChild(this._marker);
+        }
+        // Method for creating a popup window element
+        _createPopup() {
+            const popupElement = document.createElement("div");
+            popupElement.className = "popup";
+
+            const textElement = document.createElement("div");
+            textElement.className = "popup__text";
+            textElement.textContent = this._props.popupContent;
+
+            const closeBtn = document.createElement("button");
+            closeBtn.className = "popup__close";
+            closeBtn.textContent = "Close Popup";
+            closeBtn.onclick = () => {
+                this._popupOpen = false;
+                this._actualizePopup();
+            };
+
+            popupElement.append(textElement, closeBtn);
+
+            this._popup = popupElement;
+        }
+    }
 
     const map = new YMap(
         document.getElementById("app-contact"),
@@ -174,27 +258,10 @@ async function initMap() {
         ],
     });
 
-    const markerPropsVO = [
-        {
-            coordinates: [33.06208599275346, 68.95469889784452],
-            iconSrc: "https://yastatic.net/s3/front-maps-static/maps-front-jsapi-3/examples/images/marker-custom-icon/yellow-capybara.png",
-            message: "I'm yellow capybara!",
-        },
-        {
-            coordinates: [33.0767053822784, 68.96478012321226],
-            iconSrc: "https://yastatic.net/s3/front-maps-static/maps-front-jsapi-3/examples/images/marker-custom-icon/yellow-capybara.png",
-            message: "I'm yellow capybara!",
-        },
-    ];
-
     map.addChild(layer);
 
-    markerPropsVO.forEach((markerProp) => {
-        const markerElement = document.createElement("img");
-        markerElement.className = "icon-marker";
-        markerElement.src = markerProp.iconSrc;
-        markerElement.onclick = () => alert(markerProp.message);
-        map.addChild(new YMapMarker({ coordinates: markerProp.coordinates }, markerElement));
+    markerPropsVO.forEach((marker) => {
+        map.addChild(new CustomMarkerWithPopup({ coordinates: marker.coordinates, popupContent: marker.message, icon: marker.iconSrc }));
     });
 }
 
