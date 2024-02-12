38c2952a5185a2c447ec7afa00b88ce30c654b83
Гагара Николай Федорович <ggn.f764@gmail.com>
2024-02-12 17:25:39 +0300

    добавил свг маркера и ридми

--- script.js
+++ script.js
@@ -44,14 +44,12 @@ async function initMap() {
                 coordinates: [33.06618599999991, 68.95360154944407],
                 center: [33.0673554431304, 68.95425711576365],
             },
-            iconSrc:
-                "https://yastatic.net/s3/front-maps-static/maps-front-jsapi-3/examples/images/marker-custom-icon/yellow-capybara.png",
+            iconSrc: "./marker.svg",
             info: {
                 active: true,
                 name: "Мурманск, пр. Кирова, д. 1",
                 building: "./plug.png",
-                address:
-                    "183010, Мурманск, пр. Кирова, д. 1, корпус Л, каб. 112",
+                address: "183010, Мурманск, пр. Кирова, д. 1, корпус Л, каб. 112",
                 tel: "8 800 350 12 21",
                 mail: "priem@mauniver.ru",
                 opening: {
@@ -68,8 +66,7 @@ async function initMap() {
                 coordinates: [33.40823749999997, 67.5853380520345],
                 center: [33.40967293145593, 67.5859512914979],
             },
-            iconSrc:
-                "https://yastatic.net/s3/front-maps-static/maps-front-jsapi-3/examples/images/marker-custom-icon/yellow-capybara.png",
+            iconSrc: "./marker.svg",
             info: {
                 name: "Апатиты, ул. Лесная, д. 29",
                 building: "./plug.png",
@@ -93,14 +90,12 @@ async function initMap() {
                 coordinates: [33.06618599999991, 68.95360154944407],
                 center: [33.06814701625086, 68.95448301008369],
             },
-            iconSrc:
-                "https://yastatic.net/s3/front-maps-static/maps-front-jsapi-3/examples/images/marker-custom-icon/yellow-capybara.png",
+            iconSrc: "./marker.svg",
             info: {
                 active: true,
                 name: "Мурманск, ММРК им. И.И. Месяцева",
                 building: "./plug.png",
-                address:
-                    "183010, Мурманск, пр. Кирова, д. 1, корпус Л, каб. 108",
+                address: "183010, Мурманск, пр. Кирова, д. 1, корпус Л, каб. 108",
                 tel: "8 815 240 33 35",
                 mail: "priem.mmrk@mauniver.ru",
                 opening: {
@@ -116,8 +111,7 @@ async function initMap() {
                 coordinates: [33.073974499999984, 68.9635510494362],
                 center: [33.07554091006468, 68.9643258076825],
             },
-            iconSrc:
-                "https://yastatic.net/s3/front-maps-static/maps-front-jsapi-3/examples/images/marker-custom-icon/yellow-capybara.png",
+            iconSrc: "./marker.svg",
             info: {
                 name: "Мурманск, Колледж МАУ",
                 building: "./plug.png",
@@ -137,8 +131,7 @@ async function initMap() {
                 coordinates: [33.4318545, 69.204023548947],
                 center: [33.433539817903025, 69.20478506937228],
             },
-            iconSrc:
-                "https://yastatic.net/s3/front-maps-static/maps-front-jsapi-3/examples/images/marker-custom-icon/yellow-capybara.png",
+            iconSrc: "./marker.svg",
             info: {
                 name: "Филиал город Полярный",
                 building: "./plug.png",
@@ -158,13 +151,11 @@ async function initMap() {
                 coordinates: [33.675953499999984, 67.61130605196836],
                 center: [33.67729463509588, 67.61218135170577],
             },
-            iconSrc:
-                "https://yastatic.net/s3/front-maps-static/maps-front-jsapi-3/examples/images/marker-custom-icon/yellow-capybara.png",
+            iconSrc: "./marker.svg",
             info: {
                 name: "Филиал город Кировск",
                 building: "./plug.png",
-                address:
-                    "184250, г. Кировск, ул. 50 лет Октября, д. 2, корпус 1, каб. 1116",
+                address: "184250, г. Кировск, ул. 50 лет Октября, д. 2, корпус 1, каб. 1116",
                 tel: "8 815 315 54 08",
                 mail: "priem.kirovsk@mauniver.ru",
                 opening: {
@@ -300,13 +291,7 @@ async function initMap() {
         ],
     };
 
-    const {
-        YMapComplexEntity,
-        YMap,
-        YMapDefaultSchemeLayer,
-        YMapDefaultFeaturesLayer,
-        YMapMarker,
-    } = ymaps3;
+    const { YMapComplexEntity, YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker } = ymaps3;
 
     const voMap = document.getElementById("mapVo");
     const spoMap = document.getElementById("mapSpo");
@@ -366,14 +351,13 @@ async function initMap() {
             if (this._props.info.active) choiceElement.checked = true;
 
             choiceElement.onclick = (e) => {
-                const choiceElements =
-                    document.querySelectorAll(".form-check-input");
+                const choiceElements = document.querySelectorAll(".form-check-input");
 
                 const targetId = e.target.id;
 
                 choiceElements.forEach((element) => {
                     if (targetId === element.id) {
-                        element.checked = true;;
+                        element.checked = true;
                     } else {
                         element.checked = false;
                     }
@@ -408,10 +392,7 @@ async function initMap() {
                 });
             };
             container.append(marker, this._popup);
-            this._marker = new YMapMarker(
-                { coordinates: this._props.locations.coordinates },
-                container
-            );
+            this._marker = new YMapMarker({ coordinates: this._props.locations.coordinates }, container);
             this.addChild(this._marker);
         }
 
@@ -434,31 +415,15 @@ async function initMap() {
             <img src="${this._props.info.building}" />
             <ul class="list-group list-group-flush">
             <li class="list-group-item contacts__tel">
-                <a href="tel:${this._props.info.tel}" class="link">${
-                this._props.info.tel
-            }</a>
-                ${
-                    this._props.info.additional
-                        ? `<a href="tel:${this._props.info.additional}" class="link">${this._props.info.additional}</a>`
-                        : ""
-                }
+                <a href="tel:${this._props.info.tel}" class="link">${this._props.info.tel}</a>
+                ${this._props.info.additional ? `<a href="tel:${this._props.info.additional}" class="link">${this._props.info.additional}</a>` : ""}
             </li>
             <li class="list-group-item contacts__mail">
-                <a href="mailto:${
-                    this._props.info.mail
-                }" target="__blank" class="link">${this._props.info.mail}</a>
+                <a href="mailto:${this._props.info.mail}" target="__blank" class="link">${this._props.info.mail}</a>
             </li>
-            <li class="list-group-item contacts__adress">${
-                this._props.info.address
-            }</li>
-            <li class="list-group-item contacts__clock">Пн-пт: ${
-                this._props.info.opening.pt
-            } <br/>
-            ${
-                this._props.info.opening.break
-                    ? `Обед: ${this._props.info.opening.break} <br/>`
-                    : ""
-            }
+            <li class="list-group-item contacts__adress">${this._props.info.address}</li>
+            <li class="list-group-item contacts__clock">Пн-пт: ${this._props.info.opening.pt} <br/>
+            ${this._props.info.opening.break ? `Обед: ${this._props.info.opening.break} <br/>` : ""}
             Сб: ${this._props.info.opening.sb} <br/>
             Вс: ${this._props.info.opening.vs}
             </li>
@@ -481,7 +446,7 @@ async function initMap() {
         {
             location: {
                 center: [33.06618599999991, 68.95360154944407],
-                zoom: 17,
+                zoom: 18,
             },
         },
         [new YMapDefaultSchemeLayer({}), new YMapDefaultFeaturesLayer({})]
@@ -492,7 +457,7 @@ async function initMap() {
         {
             location: {
                 center: [33.06618599999991, 68.95360154944407],
-                zoom: 17,
+                zoom: 18,
             },
         },
         [new YMapDefaultSchemeLayer({}), new YMapDefaultFeaturesLayer({})]
